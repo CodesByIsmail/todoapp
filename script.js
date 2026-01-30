f882e0825c190945e6ca6040b9c231ab2871eb90
Ismail Abodunrin <ismailolakunle1401@gmail.com>
2026-01-30 17:56:47 +0100

    new updates

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const addBtn = document.querySelector('button')
 const allChecks =  document.querySelectorAll('input[type="checkbox"]')
 
+const newTaskForm = document.querySelector('.todo__task__form')
+
 const allTask = document.querySelectorAll('p')
 const todo = document.querySelectorAll('.todo')
 
@@ -10,6 +12,11 @@ const deleteOverlay = document.querySelector('.delete__overlay')
 const deleteEnquiry = document.querySelector('.delete__enquiry')
 const deleteEnquiryInputs = document.querySelectorAll('input[type="submit"]')
 
+
+const editOverlay = document.querySelector('.edit__overlay')
+const editForm = document.querySelector('.edit__form')
+const editInput = document.querySelector('.edit__input')
+
 let todoContainer  =document.querySelector('.todos')
 
 
@@ -18,9 +25,10 @@ let todoContainer  =document.querySelector('.todos')
 //     console.log(checkbtn)
 // });
 
+const allTasks = [];     
 
 const todoApp = function(){
-   const allTasks = [];
+newTaskInput.focus()
 
     const done = function(task){
         task.classList.toggle('checked')
@@ -56,26 +64,45 @@ const todoApp = function(){
     }
 
     const createNewTask = function(newTaskObj){
-        // create new div element with new task
-        const div  = document.createElement('div')
+        const html = `
+        <!-- <div class="todo__subwrapper" data-id="${newTaskObj.id}">
+        <div class="todo">
+              <input type="checkbox" name="" id="checkbox">
+              <p class="task">${newTaskObj.task}</p>
+          </div>
       
+          <div class="">
+          <i class="uil uil-edit-alt"></i>
+              <i class="uil uil-trash-alt delete"></i>
+              </div> 
+      </div> -->
+        `
 
-        div.innerHTML = `
+
+        // create new div element with new task
+        const div  = document.createElement('div');
+      div.innerHTML =
+         `
                     <div class="todo">
-                        <input type="checkbox" name="" id="checkbox">
+                        <input type="checkbox" name="" id"checkbox">
                         <p class="task">${newTaskObj.task}</p>
                     </div>
-                
-                    <div class="">
-                    <i class="uil uil-edit-alt"></i>
+                    
+                    <div class="task__icons">
+                    <i class="uil uil-edit-alt edit"></i>
                         <i class="uil uil-trash-alt delete"></i>
                         </div>
                 </div>
-        `
-
+        `;
             
         div.className = 'todo__subwrapper' // add 'todo' class to div created
+        // div.dataset('id') = newTaskObj.id
+        // div.dataset('id') = newTaskObj.id
+        div.setAttribute('data-id', newTaskObj.id)
         todoContainer.prepend(div)
+
+
+    // todoContainer.innerHTML 
 
         allTasks.push(newTaskObj);
         console.log(allTasks, 'here');  
@@ -101,74 +128,110 @@ const todoApp = function(){
 
         // create new task from new task input
         newTaskObj.task = newTaskInput.value;
+        newTaskObj.id = +((Date.now() + '').slice(-10))
 
-       createNewTask(newTaskObj)
+       createNewTask(newTaskObj);
+        const taskIcons = document.querySelector('.task__icons')
+        taskIcons.addEventListener('click', function(e){
 
+            if(e.target.className.split(' ').includes('edit')){
+                editTask(e)
+            }
 
-        const deleteBtn = document.querySelector('.delete')
-            deleteBtn.addEventListener('click', function(e){
-                const elToDelete = e.target.closest('.todo__subwrapper')
-                console.log('delete', elToDelete)
-
-                setTimeout(function(){
-                    deleteOverlay.style.display = 'block';
-
-deleteEnquiry.style.opacity = '1'
-deleteEnquiry.style.transform = 'translate(-50%, -50%)'
-
-
-
-deleteEnquiryInputs.forEach(deleteb => {
-    
-    deleteb.addEventListener('click', function(){
-        let ask = deleteb.value;
-        ask = ask.toLowerCase();
-        
-        if(ask === 'yes') {
-            elToDelete.style.transform = 'translateX(100%)'
-        elToDelete.style.opacity = 0
-            setTimeout(function(){
-                elToDelete.style.display = 'none'
-                deleteOverlay.style.display = 'none';
-
-            }, 200)
-        }
-        if(ask === 'no') {
-            deleteOverlay.style.display = 'none';
-
-        }
-    
-        console.log(deleteb)
-    })
-
-  
-                    })
-
-
-                }, 100)
-
-
-
-            })
-
-        // console.log(check);
+            if(e.target.className.split(' ').includes('delete')){
+                deleteTask(e)
+            }
+        })
     }
 
-    addBtn.addEventListener('click', function(e){
+
+    const editTask = function(e){
+
+        // const taskToEdit = e.target.parentElement.parentElement.querySelector('.task') // element to edit
+        const elToEdit = e.target.parentElement.parentElement
+allTasks.forEach(task => {
+    console.log(task.id)
+})
+        const taskToEdit = allTasks.find(task => task.id === Number(elToEdit.getAttribute('data-id')))
+        // console.log(task.id, Number(elToEdit.getAttribute('data-id')))
+        console.log(taskToEdit, Number(elToEdit.getAttribute('data-id')))
+
+        
+        editOverlay.style.display = 'block';
+        editInput.focus();
+        
+        editForm.addEventListener('submit', (e)=>{
+            e.preventDefault()
+            taskToEdit.task = editInput.value;
+            const curTaskEdit = elToEdit.querySelector('.task');
+            console.log(curTaskEdit, 'hifojflkjsdfhadslfjalidfjadsfjkkljfgslkgf')
+            curTaskEdit.textContent = taskToEdit.task
+            editInput.value = ''
+            editOverlay.style.display = 'none';
+        })
+    }
+    
+
+    const deleteTask = function(e){
+            let elToDelete = e.target.closest('.todo__subwrapper')
+            console.log('delete', elToDelete)
+
+            setTimeout(function(){
+
+                deleteOverlay.style.display = 'block';
+
+                deleteEnquiry.style.opacity = 1
+                deleteEnquiry.style.transform = 'translate(-50%, -50%)';
+
+
+                deleteEnquiryInputs.forEach(delSubmit => {
+                    delSubmit.addEventListener('click', function(){
+                        let ask = delSubmit.value;
+                        ask = ask.toLowerCase();
+        
+                        if(ask === 'yes') {
+                            elToDelete.style.transform = 'translateX(100%)'
+                            elToDelete.style.opacity = 0;
+                            console.log(elToDelete.id)
+
+                            const indexOfDeletedElement = allTasks.find(curTask => {
+                                // elToDelete.dataset('id') = curTask.id
+                                console.log(curTask)
+                                console.log(+curTask.id, +elToDelete.id, allTasks)
+
+                            })
+                            console.log(indexOfDeletedElement)
+                            setTimeout(function(){
+                                elToDelete.style.display = 'none'
+                                deleteOverlay.style.display = 'none';
+
+                            }, 200)
+                        };
+
+                        if(ask === 'no') {
+                            deleteOverlay.style.display = 'none';
+                            elToDelete = ''
+                            return
+                        };
+                    })
+                        
+                })
+
+            }, 100)
+        
+    }
+
+    newTaskForm.addEventListener('submit', function(e){
         e.preventDefault()
         if(!newTaskInput.value.trim()) return
         getNewTask()
         newTaskInput.value = ''
         newTaskInput.focus()
-
-
     })
-
-   
 }
 
 todoApp()
 
 
 // what i just learned:
-// 1. if user is adding an new element to the dom, if the element needs interaction, you will have to add it after creatioonkbo
+// 1. if user is adding an new element to the dom, if the element needs interaction, you will have to add it after creation
